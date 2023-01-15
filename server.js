1f0145b27132686ac129eb6a22d420950aab6e0e
Yulia Gladysheva <yulliagladysheva@gmail.com>
2023-01-16 00:56:33 +0300

    work on comments

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use('/', express.static(path.join(__dirname, 'dist')));
 app.use('*', express.static(path.join(__dirname, 'dist/index.html')));
